08cce1660e7c8f5363a76d31a3ba3acf17c2b517
Henrique da Silva Costa <henriquesc1442@gmail.com>
2022-05-30 08:09:27 -0300

    Update ex03/action.php

--- exercicios/ex03/action.php
+++ exercicios/ex03/action.php
@@ -1,19 +1,34 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="pt-bt">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exercício 02</title>
+    <title>Exercício 03</title>
 
     <link rel="stylesheet" href="../../css/style.css">
 </head>
 <body>
-    <h1>Exercício 02 - Analisando o número informado</h1>
+    <h1>Exercício 03 - soma entre dois números</h1>
+    
+    <p>Faça um script que peça dois números e imprima a soma.</p>
+    <hr>
+    
+    <h2>Resultado: </h2>
+    <div class="result">
+        <?php
+            # Números escolhidos pelo usuário
+            $num1 = filter_input(INPUT_POST, 'num1');
+            $num2 = filter_input(INPUT_POST, 'num2');
+            
+            # Soma entre os dois números
+            $soma = $num1 + $num2;
 
-    <?php
-       $num = filter_input(INPUT_POST, 'num');
-       echo "<p>O número digitado foi <mark>$num</mark>!</p>";
-    ?>
+            # Mostrar o resultado
+            echo "<p class='result'>• A soma entre <u>$num1</u> e <u>$num2</u> é <b>$soma</b>!";
+
+        ?>
+    </div>
+    
 </body>
 </html>
